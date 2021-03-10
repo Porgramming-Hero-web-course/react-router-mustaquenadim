2a3b1844844c81253968dbbb6a5a7e0c3f542fb5
Mustaque Nadim <mustaquenadim2000@gmail.com>
2021-03-10 12:48:30 +0600

    UI changed

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -8,12 +8,12 @@ const League = (props) => {
     // style="width: 18rem;"
     return (
         <div className="col-lg-4">
-            <div className="card text-center custom-card">
+            <div className="card text-center bg-info custom-card">
                 <div className="card-body">
                     <h5 className="card-title font-weight-bold">{strLeague}</h5>
                     <p className="card-text font-weight-normal">Sports type: {strSport}</p>
                     <Link to={`/explore/${idLeague}`}>
-                        <button className="btn btn-primary">Explore   <FontAwesomeIcon icon={faChevronRight}/></button>
+                        <button className="btn btn-success">Explore   <FontAwesomeIcon icon={faChevronRight}/></button>
                     </Link>
                 </div>
             </div>
